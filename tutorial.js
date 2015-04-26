c8d833952a5079034734a189698223f664be86dc
kubosho <ta2@o2p.jp>
2015-04-26 20:25:59 +0900

    End tutorial 2

--- tutorial.js
+++ tutorial.js
@@ -10,6 +10,26 @@ var CommentBox = React.createClass({
   }
 });
 
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I'm a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I'm a CommentForm.
+      </div>
+    );
+  }
+});
+
 React.render(
   <CommentBox />,
   document.getElementById("content")
