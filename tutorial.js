51ffd50a987f2c96de2ebed0f8829b43cc1d16ae
kubosho <ta2@o2p.jp>
2015-04-26 20:28:53 +0900

    End tutorial 3

--- tutorial.js
+++ tutorial.js
@@ -4,7 +4,9 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I'm a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
