057eef4b307c9a27a98e91c93aa8fbacddcc6882
kubosho <ta2@o2p.jp>
2015-04-26 23:51:05 +0900

    End tutorial 13

--- facebook-official/tutorial.js
+++ facebook-official/tutorial.js
@@ -1,4 +1,4 @@
-/* global marked */
+/* global marked, $ */
 
 "use strict";
 
@@ -10,6 +10,19 @@ var data = [
 var CommentBox = React.createClass({
   getInitialState: function() {
     return {data: []};
+  },
+
+  componentDidMount: function() {
+    $.ajax({
+      url     : this.props.url,
+      dataType: "json",
+      success : function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error   : function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
 
   render: function() {
